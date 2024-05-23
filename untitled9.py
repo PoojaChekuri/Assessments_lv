086dc1d0808463f5c1ca04c4ba104be88642bbb9
PoojaChekuri <165121499+PoojaChekuri@users.noreply.github.com>
2024-05-24 00:19:06 +0530

    untitled9.py

--- untitled9.py
+++ untitled9.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1SRbyMqGnn16u3E0MdNlcKdSO0KTvQ5sW
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 
